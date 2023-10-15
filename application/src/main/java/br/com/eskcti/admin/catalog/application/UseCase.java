163aabf306b1da72f08a81b6d6b85bb3c9cae4fa
eskokado <eskokado@gmail.com>
2023-10-15 15:20:58 -0300

    feat: create entity category with TDD

--- application/src/main/java/br/com/eskcti/admin/catalog/application/UseCase.java
+++ application/src/main/java/br/com/eskcti/admin/catalog/application/UseCase.java
@@ -1,9 +1,4 @@
 package br.com.eskcti.admin.catalog.application;
 
-import br.com.eskcti.admin.catalog.domain.Category;
-
 public class UseCase {
-    public Category execute() {
-        return new Category();
-    }
 }
--- domain/src/main/java/br/com/eskcti/admin/catalog/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package br.com.eskcti.admin.catalog.domain;
-
-public class Category {
-    public String id;
-}
